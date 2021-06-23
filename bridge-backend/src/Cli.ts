523e3672e548778907e1ecee284e1d345834a003
Naiem <naiemk@gmail.com>
2021-06-23 12:22:56 -0400

    Update BridgePool.json

--- bridge-backend/src/Cli.ts
+++ bridge-backend/src/Cli.ts
@@ -2,7 +2,8 @@ import { ValidationUtils } from "ferrum-plumbing";
 import { argv } from "process";
 import { processOneWay } from "./BridgeProcessor";
 console.log('Starting ');
-const net = argv[2];
+// const net = argv[2];
+const net = 'RINKEBY';
 if (!net) {
     console.log('Syntax error: include network as an argument');
 } else {
