e8e161c66bd60297d52468dc1273242c59457278
Naiem <naiemk@gmail.com>
2021-07-16 16:44:03 -0600

    Minor fix to cli.ts

--- bridge-backend/src/Cli.ts
+++ bridge-backend/src/Cli.ts
@@ -2,11 +2,11 @@ import { ValidationUtils } from "ferrum-plumbing";
 import { argv } from "process";
 import { processOneWay } from "./BridgeProcessor";
 console.log('Starting ');
-// const net = argv[2];
+const net = argv[2];
 // const net = 'RINKEBY';
 // const net = 'MUMBAI_TESTNET';
 // const net = 'BSC_TESTNET';
-const net = 'POLYGON';
+// const net = 'POLYGON';
 if (!net) {
     console.log('Syntax error: include network as an argument');
 } else {
