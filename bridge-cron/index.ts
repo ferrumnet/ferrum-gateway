b8b6ab2ab8ca197d84fbdc4e5b98fb08f0b13554
abdulahadbinkhalid <abdulahadbinkhalid@gmail.com>
2021-09-21 15:21:32 +0500

    transaction in db and cache

--- bridge-cron/index.ts
+++ bridge-cron/index.ts
@@ -5,6 +5,7 @@ import { LocalCache } from "ferrum-plumbing";
 require('dotenv').config();
 const GLOBAL_CACHE = new LocalCache();
 
+let cacheMemory = [];
 
 const options = {
   useCreateIndex: true,
@@ -12,9 +13,6 @@ const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }
-connect(process.env.MONGODB_URL, options).then(async () => {
-  console.log('Connected to MongoDB');
-});
 
 // 1. Create an interface representing a document in MongoDB.
 interface Transactions {
@@ -56,65 +54,137 @@ const transactionsSchema: Schema = new Schema <Document & Transactions>({
 // 3. Create a Model.
 const TransactionModel = model('Transaction', transactionsSchema);
 
+connect(process.env.MONGODB_URL, options).then(async () => {
+  console.log('Connected to MongoDB');
+  cacheMemory = await TransactionModel.find();
+});
 
-const getPreviousTransactions = async() => {
-  const key = "getPreviousTransactions";
-  return await GLOBAL_CACHE.getAsync(
-    key,
-    async () => {
-        const res = await TransactionModel.find().exec();
-      if (res) {
-        return res.map((r) => r.toJSON());
-      }
-    },
-  );
-};
-
-const web3Providers = [process.env.WEB3_PROVIDER_RINKEBY,process.env.WEB3_PROVIDER_ETHEREUM]
-const getWeb3Providers = async() => {
-  const key = "getWeb3Providers";
-  return await GLOBAL_CACHE.getAsync(
-    key,
-      async () => {
-        return web3Providers.map((r) => r);
-    },
-  );
-};
-
-schedule.scheduleJob('1 * * * * *', async () => {
-  let pendingTransaction = [];
-  const pendingTransactionList = await getPreviousTransactions();
-  const web3ProvidersList = await getWeb3Providers();
-  // console.log(web3ProvidersList)
-  console.log(pendingTransactionList.length)
-  if (pendingTransactionList.length) {
-    pendingTransactionList.forEach((tx: any) => {
-      web3ProvidersList.forEach(async (provider) => {
-      let web3 = new Web3(new Web3.providers.HttpProvider(provider));
-      const currentBlock = await web3.eth.getBlockNumber()
-      const web3ProviderLogs = await web3.eth.getPastLogs({ fromBlock: 1, address: "0x89262b7bd8244b01fbce9e1610bf1d9f5d97c877" })
-      web3ProviderLogs.forEach(async (log) => {
-        if (log.transactionHash !== tx.transactionHash) {
-          await web3.eth.getTransactionReceipt(log.transactionHash, async (err, result) => {
-            if (err) console.log('Error')
-            if (result) {
-              await TransactionModel.findOne({ transactionHash: result.transactionHash }, async function (err, data) {
-                // if (data) {
-                //         await Object.assign(data, result);
-                //     } else     
-                if (!err && !data) {
-                  const transaction = new TransactionModel(result);
-                  await transaction.save();
-                  GLOBAL_CACHE.set('getPreviousTransactions', transaction)
-                  const transactionList = await getPreviousTransactions();
-                  console.log(transactionList.length,'length')
-                }
-              })
-            }
-          })
-        }
-      });
-    })
-  })
+let networksCache = {
+  RINKEBY: new Web3(new Web3.providers.HttpProvider(process.env.WEB3_PROVIDER_RINKEBY)),
+  RINKEBY_WSS : new Web3(new Web3.providers.WebsocketProvider('wss://rinkeby.infura.io/ws/v3/1129bcc9a4c549ee9d5a6d98ad40133e'))
+}
+let networksBlockCache = {
+  RINKEBY: {
+    backLimit: 36000,
+    lastToBlock : undefined
   }
+}
+const getfromBlockNumber = (network:String,currentBlock:number) => {
+  let lastToBlock = networksBlockCache[`${network}`].lastToBlock;
+  if (!lastToBlock) {
+    lastToBlock = currentBlock - networksBlockCache[`${network}`].backLimit;
+  }
+  return lastToBlock;
+}
+
+const updateToBlockNumer = (toBlock:number,network:String) => {
+  networksBlockCache[`${network}`].lastToBlock = toBlock;
+}
+
+const web3Providers = [process.env.WEB3_PROVIDER_RINKEBY]
+const getWeb3Providers = async() => {
+  return ['RINKEBY'];
+};
+
+const checkCacheForTransactionStatus = (transactionHash:string) => {
+  if (cacheMemory?.length>0) {
+    const element = cacheMemory.find((item) => item.transactionHash === transactionHash);
+    if (element) {
+      return element.status;
+    }
+  }
+  return undefined;
+}
+
+const updateCacheForTransaction = (transaction: any) => {
+  const elementIndex = cacheMemory.findIndex((item) => item.transactionHash === transaction.transactionHash);
+  if (elementIndex >= 0) {
+    cacheMemory[elementIndex] = transaction;
+  } else {
+    cacheMemory.push(transaction);
+  }
+}
+
+const proccessNetworkLogs = async (logs:any,network:string) => {
+  let web3 = networksCache[`${network}`];
+  logs.forEach(async (log) => {
+    const status = checkCacheForTransactionStatus(log.transactionHash);
+      if (!status) {
+        await web3.eth.getTransactionReceipt(log.transactionHash, async (err, result) => {
+          if (err) console.log('Error')
+          if (result) {
+            await TransactionModel.findOne({ transactionHash: result.transactionHash }, async function (err, data) {
+              if (!err && !data) {
+                const transaction = new TransactionModel(result);
+                await transaction.save();
+                updateCacheForTransaction(transaction);
+              }
+            })
+          }
+        })
+      }
+    });
+  
+}
+
+// const getPendingTransactions = async () => {
+  
+//   let RINKEBY_WSS = networksCache[`RINKEBY_WSS`];
+
+
+//   RINKEBY_WSS.eth.subscribe(`pendingTransactions`, function (error, result) {
+//     if (error)
+//       console.log("Error: ", error);
+//   }).on('data', async function (TxHash) {
+//     try {
+//       let RINKEBY = networksCache[`RINKEBY`];
+//       let transaction = await RINKEBY.eth.getTransaction(TxHash);
+//       console.log(transaction,'outer')
+//       if (transaction?.to === "0x89262b7bd8244b01fbce9e1610bf1d9f5d97c877") {
+//         console.log(transaction)
+//         await RINKEBY.eth.getTransactionReceipt(transaction.hash, async (err, result) => {
+//           if (err) console.log('Error')
+//           if (result) {
+//             console.log(result)
+//             await TransactionModel.findOne({ transactionHash: result.transactionHash }, async function (err, data) {
+//               if (!err && !data) {
+//                 const transaction = new TransactionModel(result);
+//                 await transaction.save();
+//                 updateCacheForTransaction(transaction);
+//               }
+//             })
+//           }
+//         })
+//         cacheMemory.push(transaction);
+
+//       } else {
+//         console.log("external")
+//       }
+//     } catch (error) {
+//       console.log(error)
+//     }
+//   });
+// }
+
+schedule.scheduleJob('*/15 * * * * *', async () => {
+
+  const web3ProvidersList = await getWeb3Providers();
+
+  console.log(cacheMemory.length, 'cache memory');
+  // console.log(web3ProvidersList,"web3ProvidersList")
+  
+  web3ProvidersList.forEach(async (provider) => {
+    console.log('provider', provider)
+    let web3 = networksCache[`${provider}`];
+    const currentBlock = await web3.eth.getBlockNumber();
+    let fromBlock = getfromBlockNumber(provider, currentBlock);
+    
+    console.log(provider, fromBlock, currentBlock)
+    
+    if (fromBlock > 0) {
+      const web3ProviderLogs = await web3.eth.getPastLogs({ fromBlock: fromBlock, toBlock: currentBlock, address: "0x89262b7bd8244b01fbce9e1610bf1d9f5d97c877" })
+      proccessNetworkLogs(web3ProviderLogs, provider);
+    }
+    updateToBlockNumer(currentBlock, provider);
+  })
 })
