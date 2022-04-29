e7d867e438aed73438d9b20817c206bcfa950b8a
Abdul Ahad <abdulahadbinkhalid@gmail.com>
2022-04-30 01:06:20 +0500

    qp explorer ui

--- qp-explorer-frontend/src/App.tsx
+++ qp-explorer-frontend/src/App.tsx
@@ -1,21 +1,24 @@
-import React from 'react';
-import { StoreBuilder } from 'common-containers';
+import React from "react";
+import { StoreBuilder } from "common-containers";
 // import "./assets/css/icons.min.css";
 // import "bootstrap/dist/css/bootstrap.min.css";
 // import "./assets/scss/styles.scss";
 // import './App.css';
-import { Dashboard } from './pages/Dashboard';
-import { dataReducer, uiReducer, userReducer } from './common/Reducers';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { QpUiModule } from './common/QpUiModule';
-import { Environment } from 'types';
+import { Dashboard } from "./pages/Dashboard";
+import { dataReducer, uiReducer, userReducer } from "./common/Reducers";
+import { BrowserRouter as Router } from "react-router-dom";
+import { QpUiModule } from "./common/QpUiModule";
+import { Environment } from "types";
 
 const _module = new QpUiModule();
 const store = StoreBuilder.build(
-            userReducer, dataReducer, uiReducer, _module, 
-            // 'https://mz7ub9w7lj.execute-api.us-east-2.amazonaws.com/default/stagin-crucible-backend',
-            Environment.defaultEndPoint()
-            );
+  userReducer,
+  dataReducer,
+  uiReducer,
+  _module,
+  // 'https://mz7ub9w7lj.execute-api.us-east-2.amazonaws.com/default/stagin-crucible-backend',
+  Environment.defaultEndPoint()
+);
 
 function App() {
   return (
--- qp-explorer-frontend/src/pages/TransactionsList.tsx
+++ qp-explorer-frontend/src/pages/TransactionsList.tsx
@@ -1,3 +1,4 @@
+import { FCard, FContainer } from "ferrum-design-system";
 import moment from "moment";
 import { QuantumPortalRemoteTransactoin } from "qp-explorer-commons";
 import { useEffect } from "react";
@@ -7,75 +8,88 @@ import { inject, Utils } from "types";
 import { QpAppState } from "../common/QpAppState";
 import { QpExplorerClient } from "../QpExplorerClient";
 import { CHAIN_LOGO } from "./Pair";
-import './Pair.css';
+import "./Pair.css";
 
-export function TransactionDetailLine(props: {tx: QuantumPortalRemoteTransactoin}) {
-    const {tx} = props;
-    return (
-        <div className="tx-line-container">
-            <div className="tx-line-cell">
-                {CHAIN_LOGO[tx.networkId]} {' =>'} {CHAIN_LOGO[tx.remoteNetworkId]}
-            </div>
-            <div className="tx-line-cell">
-                <Link to={`/tx/${tx.hash}?network=${tx.remoteNetworkId}`} >
-                    <span>{Utils.shorten(tx.hash)}</span>
-                </Link>
-            </div>
-            <div className="tx-line-cell">
-                <span>{moment.unix(tx.timestamp).fromNow()}</span>
-            </div>
-            <div className="tx-line-cell">
-                <span>{Utils.shorten(tx.sourceBeneficiary)}</span>
-            </div>
-            <div className="tx-line-cell">
-                <span>{Utils.shorten(tx.sourceMsgSender)}</span>
-            </div>
-            <div className="tx-line-cell">
-                <span>{Utils.shorten(tx.remoteContract)}</span>
-            </div>
-            <div className="tx-line-cell">
-                <span>{tx.tokenSymbol} {tx.amountDisplay}</span>
-            </div>
-            <div className="tx-line-cell">
-                <span>{tx.gas || ''}</span>
-            </div>
-        </div>
-    )
+export function TransactionDetailLine(props: {
+  tx: QuantumPortalRemoteTransactoin;
+}) {
+  const { tx } = props;
+  return (
+    <div className="tx-line-container">
+      <div className="tx-line-cell">
+        {CHAIN_LOGO[tx.networkId]} {" =>"} {CHAIN_LOGO[tx.remoteNetworkId]}
+      </div>
+      <div className="tx-line-cell">
+        <Link
+          className={"pointer-cursor"}
+          to={`/tx/${tx.hash}?network=${tx.remoteNetworkId}`}
+        >
+          <span>{Utils.shorten(tx.hash)}</span>
+        </Link>
+      </div>
+      <div className="tx-line-cell">
+        <span>{moment.unix(tx.timestamp).fromNow()}</span>
+      </div>
+      <div className="tx-line-cell">
+        <span>{Utils.shorten(tx.sourceBeneficiary)}</span>
+      </div>
+      <div className="tx-line-cell">
+        <span>{Utils.shorten(tx.sourceMsgSender)}</span>
+      </div>
+      <div className="tx-line-cell">
+        <span>{Utils.shorten(tx.remoteContract)}</span>
+      </div>
+      <div className="tx-line-cell">
+        <span>
+          {tx.tokenSymbol} {tx.amountDisplay}
+        </span>
+      </div>
+      <div className="tx-line-cell">
+        <span>{tx.gas || ""}</span>
+      </div>
+    </div>
+  );
 }
 
 export function TransactionsList(props: {}) {
-    const {network, block} = Utils.getQueryparams();
-    const init = useSelector<QpAppState, boolean>(state => state.data.init.initialized);
-    const txs = useSelector<QpAppState, QuantumPortalRemoteTransactoin[]|undefined>(state => state.data.state.selectedBlock?.transactions);
-    const dispatch = useDispatch();
-    useEffect(() => {
-        if (network && block && init) {
-            const client = inject<QpExplorerClient>(QpExplorerClient);
-            client.blockByHash(dispatch, network, block)
-                .catch(e => console.error('Error getting block ', network, block, e));
-        }
-    }, [block, network, init]);
-    if (!txs) {
-        return (
-            <>
-            <h1>No transactions</h1>
-            </>
-        );
+  const { network, block } = Utils.getQueryparams();
+  const init = useSelector<QpAppState, boolean>(
+    (state) => state.data.init.initialized
+  );
+  const txs = useSelector<
+    QpAppState,
+    QuantumPortalRemoteTransactoin[] | undefined
+  >((state) => state.data.state.selectedBlock?.transactions);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (network && block && init) {
+      const client = inject<QpExplorerClient>(QpExplorerClient);
+      client
+        .blockByHash(dispatch, network, block)
+        .catch((e) => console.error("Error getting block ", network, block, e));
     }
+  }, [block, network, init]);
+  if (!txs) {
     return (
-        <>
-            <div className="tx-list-container">
-                <div className="tx-list-header">
-                    <b>Latest Transactions</b>
-                </div>
-                <div className="tx-list-wrapper">
-                    {txs.map((tx, i) => (
-                        <TransactionDetailLine
-                            key={i}
-                            tx={tx}
-                    />))}
-                </div>
-            </div>
-        </>
+      <>
+        <h1>No transactions</h1>
+      </>
     );
+  }
+  return (
+    <>
+      <FContainer width={1150}>
+        <FCard className="tx-list-container f-mt-4">
+          <div className="tx-list-header">
+            <b>Latest Transactions</b>
+          </div>
+          <div className="tx-list-wrapper">
+            {txs.map((tx, i) => (
+              <TransactionDetailLine key={i} tx={tx} />
+            ))}
+          </div>
+        </FCard>
+      </FContainer>
+    </>
+  );
 }
