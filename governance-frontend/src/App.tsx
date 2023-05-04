e0f1bf8643e03dc76e69a12dd7d83534a1ce90d6
Naiem Yeganeh <naiem@ferrum.network>
2023-05-03 19:00:31 -0600

    Upgrade ferrum-plumbing

--- governance-frontend/src/App.tsx
+++ governance-frontend/src/App.tsx
@@ -9,7 +9,10 @@ import { Environment } from 'types';
 
 const _module = new GovernanceModule();
 const store = StoreBuilder.build(
-            userReducer, dataReducer, uiReducer, _module, Environment.defaultEndPoint());
+            userReducer, dataReducer, uiReducer, _module, 
+            // 'https://api-gateway.dev.svcs.ferrumnetwork.io/gateway-backend-dev');
+            // Environment.defaultEndPoint());
+            'https://quwun7xfr5.us-east-2.awsapprunner.com');
             // "https://22phwrgczz.us-east-2.awsapprunner.com");
 
 function App() {
